13c9c220f78125fc816d51b092fa54314092e481
salamkhan <motivation3272@gmail.com>
2025-07-05 08:14:01 -0700

    purify out api

--- db/dbCon.js
+++ db/dbCon.js
@@ -1,12 +1,10 @@
-
 import { error } from "console";
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 
-export const dbCon=(req,res)=>{
-   try{
-    mongoose.connect("mongodb://localhost:27017/ecomApi")
-
-   }catch(err){
-     error:err.message
-   }
-}
+export const dbCon = (req, res) => {
+  try {
+    mongoose.connect(process.env.MONGO_URI);
+  } catch (err) {
+    error: err.message;
+  }
+};
